187adc6e7d6b4eb2a3475cf178ff94e7bfdb93eb
Mammad3232 <ciraqzadmmmd@gmail.com>
2026-08-27 13:29:24 +0400

    Add Frontend

--- backend/app/utils.py
+++
@@ -1,8 +0,0 @@
-from bcrypt import hashpw, gensalt, checkpw
-
-def hash_password(password: str) -> str:
-    # Şifrəni bayta çevirib bcrypt ilə hash edirik
-    return hashpw(password.encode('utf-8'), gensalt()).decode('utf-8')
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
